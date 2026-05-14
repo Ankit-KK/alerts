9aff073c980eb8a44db82a237266aa36b7f78626
Ankit Kumar <ankitashuk20@gmail.com>
2026-05-15 04:07:26 +0530

    Add sampleDonation object for DonationAlert

---
+++ src/utils/sampleDonation.ts
@@ -0,0 +1,9 @@
+import { DonationAlert } from '@/store/useDonationStore';
+
+export const sampleDonation: DonationAlert = {
+  id: 'don_001',
+  donorName: 'LOOTERIYA',
+  amount: 500,
+  message: 'Let’s gooo! 🔥',
+  currency: '₹',
+};
